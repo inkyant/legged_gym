2b8058cdd717db44ab99447b2f7ddd0808125a10
Anthony Furman <80181074+inkyant@users.noreply.github.com>
2024-07-26 08:27:16 +0000

    penalty for positional difference at no movement command

--- legged_gym/envs/b1/b1_config.py
+++ legged_gym/envs/b1/b1_config.py
@@ -67,6 +67,7 @@ class B1RoughCfg( LeggedRobotCfg ):
             # 'tracking_ang_vel': 0.5, 
             # 'tracking_lin_vel': 1.0
             # }
+            stand_still = -2.0
             pass
 
 class B1RoughCfgPPO( LeggedRobotCfgPPO ):
